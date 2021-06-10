3bc1359d8a571dbae75a3fe120e54b040e99551e
Aleksandr Lyapunov <alyapunov@tarantool.org>
2021-06-10 13:01:29 +0300

    improve run

--- run.lua
+++ run.lua
@@ -7,6 +7,7 @@ ffi = require('ffi')
 log = require('log')
 fiber = require('fiber')
 netbox = require('net.box')
+clock = require('clock')
 txn_proxy = require('txn_proxy')
 my = require('my')
 
@@ -24,7 +25,7 @@ my.init()
 --my.create_space{engine = 'vinyl'}
 my.create_space{engine = 'memtx'}
 my.create_index{type='tree', parts={{1, 'uint'}}}
-my.create_index{type='tree', parts={{2, 'uint'}}}
+my.create_index{type='hash', parts={{2, 'uint'}}}
 --my.create_index{type='tree', parts={{2, 'uint'}}, unique=true}
 --parts = {{field = "attributes", is_nullable = true, path = "name[*].key", type = "string"}}
 --parts = {{field = 2, path = "[*].key", type = "string"}, {field = 2, path = "[*].name", type = "string"}}
@@ -34,6 +35,7 @@ my.create_index{type='tree', parts={{2, 'uint'}}}
 
 tx1 = txn_proxy.new()
 tx2 = txn_proxy.new()
+tx3 = txn_proxy.new()
 --tx1:begin() tx1("s:replace{1,1}") tx1:commit()
 
 
@@ -44,8 +46,9 @@ tx2 = txn_proxy.new()
 function bench_call_echo(...)  print(fiber.id(), "bench_call_echo", my.tostring(...)) return ... end
 function test(...) my.print({...}) return 1, {2, 3}, 4 end
 
-box.schema.user.grant('guest', 'execute', 'universe')
-box.schema.user.grant('guest', 'read,write', 'space', 'test')
+box.schema.user.grant('guest', 'execute', 'universe', nil, {if_not_exists=true})
+box.schema.user.grant('guest', 'read,write', 'space', 'test', nil, {if_not_exists=true})
+box.schema.user.grant('guest', 'read,write', 'universe', nil, {if_not_exists=true})
 
 box.schema.func.create('echo', {language='C', if_not_exists=true})
 box.schema.user.grant('guest', 'execute', 'function', 'echo')
@@ -54,7 +57,7 @@ box.schema.func.create('vshard.storage.call', {language='C', if_not_exists=true}
 box.schema.user.grant('guest', 'execute', 'function', 'vshard.storage.call')
 -- unpack(unpack(conn:call('vshard.storage.call', {3500, 'read', 'bench_call_echo', {{1, 2, 3}}})))
 
-conn = netbox:connect("localhost:3301")
+conn = netbox:connect(box.cfg.listen)
 
 --local s = conn.space[512]
 --s:replace{12345, 'asd', false}
