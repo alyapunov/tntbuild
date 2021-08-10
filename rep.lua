ce9f419df0d193371b7ea9a2620ccd6b7a87f4f2
Aleksandr Lyapunov <alyapunov@tarantool.org>
2021-08-10 11:19:50 +0300

    update rep.lua

--- rep.lua
+++ rep.lua
@@ -17,7 +17,7 @@ my = require('my')
 
 --box.cfg{wal_mode='none', memtx_memory=1024*1024*1024, listen=3301}
 --box.cfg{wal_mode='write', memtx_memory=1024*1024*1024, listen=3301, log_level=7}
-box.cfg{wal_mode='write', memtx_memory=1024*1024*1024, listen=3301, memtx_use_mvcc_engine=true, log_level=5}
+box.cfg{wal_mode='write', memtx_memory=107374182, listen=3301, memtx_use_mvcc_engine=true, log_level=5}
 
 txn_proxy = require('txn_proxy')
 
@@ -523,6 +523,74 @@ s:truncate()
 
 i2 = s:create_index('sk', {type = 'hash', parts={2,'unsigned'}})
 
+legend = 'status        i1:count() i2:count()'
+legend = legend .. ' tx1:i1:count() tx1:i2:count()'
+legend = legend .. ' tx2:i1:count() tx2:i2:count()'
+function check()                                                         \
+    local res = ''                                                       \
+    local ok = true                                                      \
+    res = res .. '        ' .. tostring(i1:count())                      \
+    if (i1:count() ~= #i1:select{}) or (i1:len() ~= #i1:select{}) then   \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+    res = res .. '        ' .. tostring(i2:count())                      \
+    if (i2:count() ~= #i2:select{}) or (i1:len() ~= #i1:select{}) then   \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+    res = res .. '        ' .. tostring(tx1('i1:count()')[1])            \
+    if (tx1('i1:count()')[1] ~= tx1('#i1:select{}')[1]) or               \
+       (tx1('i1:len()')[1] ~= tx1('#i1:select{}')[1]) then               \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+    res = res .. '        ' .. tostring(tx1('i2:count()')[1])            \
+    if (tx1('i2:count()')[1] ~= tx1('#i2:select{}')[1]) or               \
+       (tx1('i2:len()')[1] ~= tx1('#i2:select{}')[1]) then               \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+    res = res .. '        ' .. tostring(tx2('i1:count()')[1])            \
+    if (tx2('i1:count()')[1] ~= tx2('#i1:select{}')[1]) or               \
+       (tx2('i1:len()')[1] ~= tx2('#i1:select{}')[1]) then               \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+    res = res .. '        ' .. tostring(tx2('i2:count()')[1])            \
+    if (tx2('i2:count()')[1] ~= tx2('#i2:select{}')[1]) or               \
+       (tx2('i2:len()')[1] ~= tx2('#i2:select{}')[1]) then               \
+        ok = false                                                       \
+        res = res .. '!'                                                 \
+    end                                                                  \
+									 \
+    if ok then                                                           \
+        res = 'ok' .. res                                                \
+    else                                                                 \
+        res = 'fail' .. res                                              \
+    end                                                                  \
+    return res                                                           \
+end
+legend
+
+s:replace{1, 2}
+s:replace{3, 4}
+tx1:begin()
+tx2:begin()
+check()
+tx1('s:replace{5, 42}')
+check()
+tx2('s:replace{6, 42}')
+check()
+tx1('s:delete{1}')
+tx2('s:delete{3}')
+check()
+tx1('s:replace{8, 2}')
+tx2('s:replace{3, 8}')
+check()
+tx1:commit()
+tx2:commit()
+
 -- Check different orders
 s:truncate()
 tx1:begin()
@@ -564,42 +632,56 @@ tx2('s:replace{1, 2}')
 tx2:commit()
 tx1:commit()
 
-test_run:cmd("setopt delimiter ';'")
-run_background_mvcc = true
-function background_mvcc()
-    while run_background_mvcc do
-        box.space.accounts:update('petya', {{'+', 'balance', math.ceil(math.random() * 200) - 100}})
-    end
-end
-test_run:cmd("setopt delimiter ''");
-
-_ = box.schema.space.create('accounts', { format = {'name', 'balance'} })
-_ = box.space.accounts:create_index('pk', { parts = { 1, 'string' } })
-box.space.accounts:insert{ 'vasya', 0 }
-box.space.accounts:insert{ 'petya', 0 }
-
-fiber = require 'fiber'
-
+-- https://github.com/tarantool/tarantool/issues/6131
+s:truncate()
+s:replace{1, 1}
 tx1:begin()
-tx1("box.space.accounts:update('vasya', {{'=', 'balance', 10}})")
-
 tx2:begin()
-tx2("box.space.accounts:update('vasya', {{'=', 'balance', 20}})")
+tx1('s:select{1}')
+tx2("s:update({1}, {{'=', 2, 2}})")
 tx2:commit()
-
-fib = fiber.create(background_mvcc)
-fib:set_joinable(true)
-fiber.sleep(0.1)
-run_background_mvcc = false
-fib:join();
-
+tx1("s:update({1}, {{'=', 2, 3}})")
 tx1:commit()
-box.space.accounts:select{'vasya'}
-box.space.accounts:drop()
 
 s:drop()
 
+-- https://github.com/tarantool/tarantool/issues/6140
+s3 = box.schema.space.create('test3')
+i31 = s3:create_index('pk', {parts={{1, 'uint'}}})
+tx1:begin()
+tx1('s3:replace{2}')
+tx1('s3:select{}')
+s3:drop()
+tx1:rollback()
+
+-- gh-6095: SQL query may crash in MVCC mode if it involves ephemeral spaces.
+--
+box.execute([[ CREATE TABLE test (id INT NOT NULL PRIMARY KEY, count INT NOT NULL)]])
+box.execute([[ UPDATE test SET count = count + 1 WHERE id = 0 ]])
+box.execute([[ DROP TABLE test]])
+
 -- https://github.com/tarantool/tarantool/issues/5515
+NUM_TEST_SPACES = 4
+sp = {}
+for k=1,NUM_TEST_SPACES do				\
+	sp[k] = box.schema.space.create('test' .. k)	\
+	sp[k]:create_index('test1', {parts={1}})	\
+	sp[k]:create_index('test2', {parts={2}})	\
+end
+for k = 1,NUM_TEST_SPACES do				\
+	for i = 1,100 do				\
+		collectgarbage('collect')		\
+		box.begin()				\
+		sp[i % k + 1]:replace{1, 1, "hi", i}	\
+		sp[i % k + 1]:delete{1}			\
+		box.commit()				\
+	end						\
+end
+for k=1,NUM_TEST_SPACES do				\
+	sp[k]:drop()					\
+end
+
+-- Test all index types
 s = box.schema.space.create('test')
 i0 = s:create_index('pk', {parts={{1, 'uint'}}})
 i1 = s:create_index('i1', {id = 10, type = 'tree', parts={{2, 'uint'}}})
@@ -612,28 +694,39 @@ s:replace{1, 2, 3, {2, 2}}
 tx1:begin()
 tx1('i1:select{2}')
 tx1('i1:select{3}')
+tx1('i1:count()')
 tx1('i2:select{2}')
 tx1('i2:select{3}')
+tx1('i2:count()')
 tx1('i3:select{3}')
 tx1('i3:select{16}')
+tx1('i3:count()')
 tx1('i4:select{2, 2}')
 tx1('i4:select{3, 3}')
+tx1('i4:count()')
 tx1:commit()
 s:drop()
 
-box.begin()
-s = box.schema.space.create('test')
-_ = box.space.test:create_index('pk')
-box.space.test:replace({1,2,3})
-tx1:begin()
-tx1('s:replace{2,2,3}');
-tx1('s:replace{3,2,3}');
-tx1:commit()
-box.space.test:truncate()
-assert(box.space.test ~= nil)
-box.rollback()
+-- CASE 2 from #5515
+fiber = require('fiber')
+channel1 = fiber.channel()
+channel2 = fiber.channel()
+function test1()                                 \
+    box.begin()                                  \
+    local s = box.schema.space.create('test')    \
+    s:create_index('pk')                         \
+    channel1:put(1)                              \
+    local ok = channel2:get()                    \
+    box.session.push(ok)                         \
+    box.rollback()                               \
+    channel1:put(1)                              \
+end
+f = fiber.new(test1)
+channel1:get()
+
 assert(box.space.test == nil)
-collectgarbage()
+channel2:put(1)
+channel1:get()
 assert(box.space.test == nil)
 
 -- https://github.com/tarantool/tarantool/issues/6137
@@ -747,7 +840,7 @@ s:drop()
 -- More complex conflict in secondary index.
 s = box.schema.create_space('test')
 pk = s:create_index('pk', {parts={1, 'uint'}})
-sk = s:create_index('pk', {parts={2, 'uint'}})
+sk = s:create_index('sk', {parts={2, 'uint'}})
 tx1:begin()
 tx2:begin()
 tx1('s:replace{1, 1, 1}')
@@ -762,7 +855,7 @@ s:drop()
 
 s = box.schema.create_space('test')
 pk = s:create_index('pk', {parts={1, 'uint'}})
-sk = s:create_index('pk', {parts={2, 'uint'}})
+sk = s:create_index('sk', {parts={2, 'uint'}})
 tx1:begin()
 tx2:begin()
 tx1('s:replace{1, 1, 1}')
@@ -807,14 +900,13 @@ s:select{}
 s:drop()
 
 --https://github.com/tarantool/tarantool/issues/6132
-test_run:cmd("setopt delimiter ';'")
 run_background_mvcc = true
-function background_mvcc()
-    while run_background_mvcc do
-        box.space.accounts:update('petya', {{'+', 'balance', math.ceil(math.random() * 200) - 100}})
-    end
+function background_mvcc()                                              \
+    while run_background_mvcc do                                        \
+        local bal = math.ceil(math.random() * 200) - 100                \
+        box.space.accounts:update('petya', {{'+', 'balance', bal}})     \
+    end                                                                 \
 end
-test_run:cmd("setopt delimiter ''");
 
 _ = box.schema.space.create('accounts', { format = {'name', 'balance'} })
 _ = box.space.accounts:create_index('pk', { parts = { 1, 'string' } })
@@ -859,6 +951,83 @@ tx2("s:replace({1, 3})")
 tx2:commit()
 tx1:commit()
 
+s:drop()
+
+s = box.schema.create_space('test')
+_ = s:create_index('pk')
+tx1:begin()
+tx1('s:select{}')
+tx2:begin()
+tx2('s:replace{2, 2, 2}')
+tx3:begin()
+tx3('s:replace{1, 1, 1}')
+tx3:commit()
+tx1('s:select{}')
+tx1:commit();
+tx2:rollback()
+s:drop()
+
+--https://github.com/tarantool/tarantool/issues/6247
+s = box.schema.create_space('test')
+pk = s:create_index('pk', {parts={1, 'uint'}})
+sk = s:create_index('sk', {parts={2, 'uint'}})
+
+-- Make lots of changes to ensure that GC make the first tuple {1, 1,1} clean.
+collectgarbage('collect')
+for i = 1,10 do s:replace{i, i, i} end
+collectgarbage('collect')
+for i = 11,100 do s:replace{i, i, i} end
+-- Make a new tuple that is definitely dirty now.
+s:replace{0, 0, 0}
+-- Hit dirty {0, 0, 0} in pk and conflict clean {1, 1, 1}.
+s:replace{0, 1, 0}
+
+s:drop()
+
+--https://github.com/tarantool/tarantool/issues/6206
+spc = box.schema.space.create('test')
+_ = spc:create_index('test', {type='tree'})
+
+tx1:begin()
+tx1('spc:select{}')
+spc:replace{1, 1}
+tx1('spc:select{}')
+tx1:commit()
+
+spc:delete{1}
+tx2 = txn_proxy.new()
+tx2:begin()
+tx2('spc:select{}')
+spc:replace{1, 1}
+tx2('spc:select{}')
+tx2:commit()
+
+spc:drop()
+
+-- Check possible excess conflict: reference test
+s = box.schema.create_space('test')
+primary = s:create_index('pk', {parts={1, 'uint'}})
+secondary = s:create_index('sk', {parts={2, 'uint'}})
+tx1:begin()
+tx1('secondary:select{1}')
+s:replace{1, 2}
+tx1('primary:select{1}')
+tx1('s:replace{3, 3}')
+tx1:commit()
+s:drop()
+
+-- Check possible excess conflict: test with deleted story
+s = box.schema.create_space('test')
+primary = s:create_index('pk', {parts={1, 'uint'}})
+secondary = s:create_index('sk', {parts={2, 'uint'}})
+s:replace{1, 1} -- The difference from the test above is that the tuple
+s:delete{1}     -- is inserted and immediately deleted.
+tx1:begin()
+tx1('secondary:select{1}')
+s:replace{1, 2}
+tx1('primary:select{1}')
+tx1('s:replace{3, 3}')
+tx1:commit()
 s:drop()
 
 require('console').start()
