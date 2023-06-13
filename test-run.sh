efb9c702fc7261cb4c650b7dadeba3ebdfd1172d
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-06-13 17:47:27 +0300

    remove --vardir option from test-run.sh

--- test-run.sh
+++ test-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MY_BUILD_DIR="$(pwd)"
 for i in {1..16}; do
-    cd "$MY_BUILD_DIR/test" && "$MY_BUILD_DIR/../test/test-run.py" "--builddir=$MY_BUILD_DIR" "--vardir=$MY_BUILD_DIR/test/var" --force $@
+    cd "$MY_BUILD_DIR/test" && "$MY_BUILD_DIR/../test/test-run.py" "--builddir=$MY_BUILD_DIR" --force $@
     cd "$MY_BUILD_DIR"
 
     if ! [[ -f /tmp/luatest_auto.txt ]]; then
