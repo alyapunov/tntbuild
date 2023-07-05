9ea5a0a4d844485c86cd519a2e84dfbe8ad85072
Aleksandr Lyapunov <alyapunov@tarantool.org>
2023-07-05 19:14:17 +0300

    clean non-targeted unit test in test-run.sh

--- test-run.sh
+++ test-run.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 MY_BUILD_DIR="$(pwd)"
+
+UNIT_TESTS="$(pwd)/test/unit/*.test"
+dropped_unit_tests=()
+for file in $UNIT_TESTS; do
+    if [[ -x "$file" ]]; then
+        make -q $(basename "$file") 2> /dev/null
+        rc=$?
+        if [[ $rc == 2 ]]; then
+            dropped_unit_tests+=($(basename "$file"))
+            rm "$file"
+        fi
+    fi
+done
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+if ! [[ ${#dropped_unit_tests[@]} == 0 ]]; then
+    echo "The following unit tests were dropped since they are not in targets"
+    for file in ${dropped_unit_tests[@]}; do
+        printf "${RED}$file${NC}\n"
+    done
+fi
+
 for i in {1..16}; do
     cd "$MY_BUILD_DIR/test" && "$MY_BUILD_DIR/../test/test-run.py" "--builddir=$MY_BUILD_DIR" --force $@
     cd "$MY_BUILD_DIR"
