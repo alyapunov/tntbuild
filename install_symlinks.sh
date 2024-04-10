125031a794a0b89a27bcbc8fe48c517687da5669
Aleksandr Lyapunov <alyapunov@tarantool.org>
2024-04-10 18:58:27 +0300

    update install_symlinks.sh to work with ee

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if ! [[ -f $(pwd)/src/tarantool ]]; then
+if ! [[ -f $(pwd)/src/tarantool ]] && ! [[ -f $(pwd)/tarantool/src/tarantool ]]; then
     echo "Must be run from tarantool build dirctory!"
     exit 1
 fi
 
-if ! grep -i 'project(tarantool' ../CMakeLists.txt > /dev/null; then
-    echo "Must be run from tarantool build dirctory!"
-    exit 1
+if [[ -f $(pwd)/src/tarantool ]]; then
+    ln -s -f $(pwd)/src/tarantool ~/bin/tarantool
+elif [[ -f $(pwd)/tarantool/src/tarantool ]]; then
+    ln -s -f $(pwd)/tarantool/src/tarantool ~/bin/tarantool
 fi
 
-ln -s -f $(pwd)/src/tarantool ~/bin/tarantool
-ln -s -f $(pwd)/extra/dist/tarantoolctl ~/bin/tarantoolctl
+if [[ -f $(pwd)/extra/dist/tarantoolctl ]]; then
+    ln -s -f $(pwd)/extra/dist/tarantoolctl ~/bin/tarantoolctl
+elif [[ -f $(pwd)/tarantool/extra/dist/tarantoolctl ]]; then
+    ln -s -f $(pwd)/tarantool/extra/dist/tarantoolctl ~/bin/tarantoolctl
+fi
+
 
