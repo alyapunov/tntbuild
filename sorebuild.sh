b8525d9f688d42b9b829e47cdb34561cbefc3097
Aleksandr Lyapunov <alyapunov@tarantool.org>
2024-06-06 14:52:37 +0300

    fix sorebuild.sh

--- sorebuild.sh
+++ sorebuild.sh
@@ -5,7 +5,7 @@ TNT=$(pwd)/..
 
 MP="$TNT/src/lib/msgpuck"
 LJ="$TNT/third_party/luajit/src"
-I="-I./so -I$BLD/src -I../third_party/luajit/src -I$LJ -I$MP"
+I="-I./so -I$BLD/src -I$LJ -I$MP"
 
 gcc ./so/so.c "$MP/msgpuck.c" "$MP/hints.c" $I --shared -fPIC -o ./echo.so
 gcc ./so/so.c "$MP/msgpuck.c" "$MP/hints.c" $I --shared -fPIC -o ./vshard/storage.so
